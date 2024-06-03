03804874c7201d2ac55b3375abdc007a1cc396a9
Justin Clayton <justinclayton@users.noreply.github.com>
2024-06-02 23:20:46 -0700

    first pass of dummy workspace for testing 2

--- 0_control-workspace/common-variables.tf
+++ 0_control-workspace/common-variables.tf
@@ -2,17 +2,17 @@ variable "aws_account_id" {
   type = string
 }
 
-variable "boundary_admin_password" {
-  type = string
-}
+# variable "boundary_admin_password" {
+#   type = string
+# }
 
 variable "my_email" {
   type = string
 }
 
-variable "nomad_license" {
-  type = string
-}
+# variable "nomad_license" {
+#   type = string
+# }
 
 variable "region" {
   type = string
