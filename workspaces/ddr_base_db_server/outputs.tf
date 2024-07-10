1094dad41084256af07f9fc4c6553fc4f197b6a2
Justin Clayton <justinclayton@users.noreply.github.com>
2024-07-10 11:14:50 -0700

    fixing outputs for ddr_base_db_server

--- workspaces/ddr_base_db_server/outputs.tf
+++ workspaces/ddr_base_db_server/outputs.tf
@@ -1,19 +1,27 @@
-output "hvn_id" {
-  value = hcp_hvn.main.hvn_id
+# output "hvn_id" {
+#   value = hcp_hvn.main.hvn_id
+# }
+
+# output "vpc_id" {
+#   value = module.vpc.vpc_id
+# }
+
+# output "subnet_ids" {
+#   value = module.vpc.public_subnets
+# }
+
+# output "subnet_cidrs" {
+#   value = module.vpc.public_subnets_cidr_blocks
+# }
+
+# output "hvn_sg_id" {
+#   value = module.aws_hcp_network_config.security_group_id
+# }
+
+output "db_server_private_ip" {
+  value = aws_instance.database.private_ip
 }
 
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "subnet_ids" {
-  value = module.vpc.public_subnets
-}
-
-output "subnet_cidrs" {
-  value = module.vpc.public_subnets_cidr_blocks
-}
-
-output "hvn_sg_id" {
-  value = module.aws_hcp_network_config.security_group_id
+output "db_server_public_ip" {
+  value = aws_instance.database.public_ip
 }
