86710ad45b98a76493ed24e421c1fbf6bbb62f3e
Justin Clayton <justinclayton@users.noreply.github.com>
2024-07-11 12:25:54 -0700

    2x retry: adding rds aurora postgres instead because i know how to get that working

--- workspaces/ddr_base_db_server/main.tf
+++ workspaces/ddr_base_db_server/main.tf
@@ -79,7 +79,7 @@ resource "aws_rds_cluster" "db" {
   master_username      = "foo"
   master_password      = "barbut8chars"
   engine               = "aurora-postgresql"
-  db_subnet_group_name = aws_db_subnet_group.db
+  db_subnet_group_name = aws_db_subnet_group.db.name
   skip_final_snapshot  = true
   vpc_security_group_ids = [ aws_security_group.database.id ]
 }
